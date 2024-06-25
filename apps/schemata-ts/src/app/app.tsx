c2bbe4fe253ce866f7ad317e0ebedd5072b0fcc5
Igor Loskutov <igor.loskutoff@gmail.com>
2024-06-25 17:58:00 -0400

    Initial commit

---
+++ apps/schemata-ts/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="schemata-ts" />
+    </div>
+  );
+}
+
+export default App;
