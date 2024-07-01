76e7214ed3ebebc85fee5d8b4d026d9425eccee4
Igor Loskutov <igor.loskutoff@gmail.com>
2024-07-01 19:37:51 -0400

    yup scaffold

---
+++ libs/yup/src/lib/yup.ts
@@ -0,0 +1,21 @@
+import { object, string, number, date, InferType } from 'yup';
+
+let userSchema = object({
+  name: string().required(),
+  age: number().required().positive().integer(),
+  email: string().email(),
+  website: string().url().nullable(),
+  createdOn: date().default(() => new Date()),
+});
+
+// parse and assert validity
+const user = userSchema
+
+type User = InferType<typeof userSchema>;
+/* {
+  name: string;
+  age: number;
+  email?: string | undefined
+  website?: string | null | undefined
+  createdOn: Date
+}*/
