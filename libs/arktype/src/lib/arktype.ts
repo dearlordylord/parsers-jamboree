4b527a94f5ab7f7dc6dbe4a71ddfd2929246c28c
Igor Loskutov <igor.loskutoff@gmail.com>
2024-07-27 21:13:34 -0400

    adjustments + arktype todos

--- libs/arktype/src/lib/arktype.ts
+++ libs/arktype/src/lib/arktype.ts
@@ -1,29 +1,13 @@
 import { ArkErrors, type } from 'arktype';
 import {
   chain,
-  COLOURS,
+  COLOURS, ISO_DATE_REGEX,
   Result,
   SUBSCRIPTION_TYPES,
-  TrustedCompileTimeMeta,
+  TrustedCompileTimeMeta
 } from '@parsers-jamboree/common';
 import { Objects, Pipe, Strings, Tuples } from 'hotscript';
 import Mutable = Objects.Mutable;
-
-/* TODO explore narrowing
-const palindromicEmail = type("email").narrow((address, ctx) => {
-	if (address === [...address].reverse().join("")) {
-		// congratulations! your email is somehow a palindrome
-		return true
-	}
-	// add a customizable error and return false
-	return ctx.mustBe("a palindrome")
-})
-
-const palindromicContact = type({
-	email: palindromicEmail,
-	score: "integer < 100"
-})
- */
 
 // had to use hotscript to type the literal union imported from a const array
 type TupleToLiteral<T extends readonly unknown[]> = Pipe<
@@ -51,26 +35,49 @@ const hexColorRegexString = `^#[a-fA-F0-9]{6}$`;
 const COLOURS_WITH_CODES_LITERAL =
   `(${COLOURS_LITERAL})|/${hexColorRegexString}/` as const;
 
+const isoDateString = type('string').narrow((s, ctx) => {
+  console.log('testing ' , s)
+  if (!ISO_DATE_REGEX.test(s))
+    return ctx.mustBe('a valid ISO date string');
+  return true;
+});
+
+const favouriteColours = type(`(${COLOURS_WITH_CODES_LITERAL})[]`).narrow((v, ctx) => {
+  const set = new Set(v);
+  if (set.size !== v.length) {
+    return ctx.mustBe('favourite colours must be unique');
+  }
+  return true;
+});
+
+// TODO
+const fileSystem = type('any');
+// TODO
+const profile = type('any');
+
 const userJson = type({
   name: '0<string<255',
   email: 'email',
   // https://github.com/arktypeio/arktype/issues/909 morphs aren't really here yet
-  createdAt: 'string',
-  updatedAt: 'string',
+  createdAt: isoDateString,
+  updatedAt: isoDateString,
   subscription: SUBSCRIPTION_TYPES_LITERAL,
   stripeId: /^cus_[a-zA-Z0-9]{14,}$/,
   visits: 'integer>0',
   // https://github.com/arktypeio/arktype/issues/909 morphs aren't really here yet
-  favouriteColours: `(${COLOURS_WITH_CODES_LITERAL})[]`,
+  favouriteColours,
+  profile,
+  fileSystem,
+}).narrow((u, ctx) => {
+  const createdAt = new Date(u.createdAt);
+  const updatedAt = new Date(u.updatedAt);
+  if (updatedAt < createdAt) {
+    return ctx.mustBe('createdAt must be less or equal than updatedAt');
+  }
+  return true;
 });
 
 type UserJson = typeof userJson.infer;
-
-// type User = Omit<UserJson, 'favouriteColours' | 'createdAt' | 'updatedAt'> & {
-//   createdAt: Date;
-//   updatedAt: Date;
-//   favouriteColours: Set<string>;
-// };
 
 type User = UserJson;
 
@@ -78,26 +85,8 @@ export const decodeUser = (u: unknown): Result<string, User> => {
   const result = userJson(u);
 
   return chain((u: UserJson): Result<string, User> => {
-    // const favouriteColours = new Set(u.favouriteColours);
-    // if (favouriteColours.size !== u.favouriteColours.length) {
-    //   return { _tag: 'left', error: 'favourite colours must be unique' };
-    // }
-    // const createdAt = new Date(u.createdAt);
-    // if (isNaN(createdAt.getTime())) {
-    //   return { _tag: 'left', error: 'createdAt must be a valid ISO date' };
-    // }
-    // const updatedAt = new Date(u.updatedAt);
-    // if (isNaN(updatedAt.getTime())) {
-    //   return { _tag: 'left', error: 'updatedAt must be a valid ISO date' };
-    // }
     return {
       _tag: 'right',
-      // value: {
-      //   ...u,
-      //   favouriteColours,
-      //   createdAt,
-      //   updatedAt,
-      // },
       value: u,
     };
   })(mapResult(result));
