330e09e03e8710586acdca4650c788eef314d033
Igor Loskutov <igor.loskutoff@gmail.com>
2024-08-22 18:19:32 -0400

    template literals "ON" for typebox

--- libs/typebox/src/lib/meta.ts
+++ libs/typebox/src/lib/meta.ts
@@ -4,7 +4,7 @@ export const meta: TrustedCompileTimeMeta = {
   items: {
     branded: false,
     typedErrors: false,
-    templateLiterals: false,
+    templateLiterals: true,
     emailFormatAmbiguityIsAccountedFor: true,
     acceptsTypedInput: false,
     canGenerateJsonSchema: true,
@@ -12,7 +12,7 @@ export const meta: TrustedCompileTimeMeta = {
   explanations: {
     branded: 'Can be simulated with decode/encode but no native support',
     templateLiterals:
-      'Can be simulated with decode/encode but no native support',
+      'https://github.com/sinclairzx81/typebox?tab=readme-ov-file#template-literal-types',
     typedErrors:
       "Value.Check() + Value.Errors() can be used, but it loses transformations; features don't compose so I have to fail one of them arbitrarily.",
     emailFormatAmbiguityIsAccountedFor: `A default email format check method doesn't work so I assume it's not defined at all. See also Ajv email explanation for more context.`,
