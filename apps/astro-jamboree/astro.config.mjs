802ca4615b302f78b8ea5f7df6586668a3a82352
Igor Loskutov <igor.loskutoff@gmail.com>
2024-09-19 22:32:59 -0400

    add sitemap

--- apps/astro-jamboree/astro.config.mjs
+++ apps/astro-jamboree/astro.config.mjs
@@ -2,16 +2,23 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 //import Icons from "unplugin-icons/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.typescript-validators.com',
   outDir: '../../dist/apps/astro-jamboree',
   integrations: [react(), tailwind({
     configFile: './apps/astro-jamboree/tailwind.config.mjs',
-  }), mdx()],
+  }), mdx(), sitemap()],
   // output: "server",
-  // vite: {
+  vite: {
+    ssr: {
+      external: ['react', 'react-dom'],
+      noExternal: true // somehow this way astro sitemap doesn't fail
+    }
+  }
   //   plugins: [
   //     Icons({
   //       jsx: "react",
